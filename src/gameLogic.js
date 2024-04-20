de0db335b35ef20eef9af74b49533ebc7ebdec11
cabelstein12 <Caleb.Steinwandt@gmail.com>
2024-04-19 21:15:46 -0600

    fix error when re-selecting an attacked coordinate

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,108 +1,121 @@
-    class Ship {
-        constructor(name, length){
-            this.name = name;
-            this.length = length;
-            this.timesHit = 0;
-            this.sunk = false;
+class Ship {
+    constructor(name, length){
+        this.name = name;
+        this.length = length;
+        this.timesHit = 0;
+        this.sunk = false;
+    }
+    hit(){
+        if(!this.sunk){this.timesHit++}
+        this.isSunk()
+    }
+    isSunk(){
+        if(this.timesHit == this.length){
+            this.sunk = true;
+            // console.log(this);
+            // console.log(`Our ${this.name} has sunk, Captain!`);
         }
-        hit(){
-            if(!this.sunk){this.timesHit++}
-            this.isSunk()
-        }
-        isSunk(){
-            if(this.timesHit == this.length){
-                this.sunk = true;
-                console.log(this);
-                console.log(`Our ${this.name} has sunk, Captain!`);
+    }
+}
+
+class GameBoard{
+    constructor(name){
+        this.name = name;
+        this.grid = [];
+        for(let x = 0; x < 10; x++){
+            for(let y = 0; y < 10; y++){
+                this.grid.push([x,y]);
             }
         }
     }
-    
-    class GameBoard{
-        constructor(name){
-            this.name = name;
-            this.grid = [];
-            for(let x = 0; x < 10; x++){
-                for(let y = 0; y < 10; y++){
-                    this.grid.push([x,y]);
-                }
-            }
-        }
-        
-        ships = [
-            new Ship('Carrier', 5),
-            new Ship('Battleship', 4),
-            new Ship('Cruiser', 3),
-            new Ship('Submarine', 3),
-            new Ship('Destroyer', 2)
-        ]
-        placeShip(startIndex, direction, ship){
-            let board = this;
-            function checkValid(){
-                if(direction == "horizontal"){
-                    if(board.grid[startIndex + ship.length] !== undefined){
-                        return true;
-                    }
-                }
-                if(direction == "vertical"){
-                    if(board.grid[(startIndex + 10) * ship.length] !== undefined){
-                        return true;
-                    }
-                }
-                return false;
-            }
-            let lengthCount = ship.length;
-            let start = startIndex
-            while(lengthCount > 0){
-                this.grid[start].push(ship);
-                if(direction == 'horizontal' && checkValid()){
-                    start++;
-                }
-                if(direction == 'vertical' && checkValid){
-                    start = start + 10
-                }
-                lengthCount--;
-            }
-        }
-        missedShotLog = [];
-        casualties = [];
-        receiveAttack(coord){
-    
-            let ship = this.grid[coord][2];
-            if(this.grid[coord][2] !== undefined){
-                ship.hit();
-                if(ship.sunk){this.casualties.push(ship)};
-                this.grid[coord].push('Hit!')
-                console.log('Hit!')
-                return "Hit!"
-            }else{
-                this.grid[coord][2] = 'miss'
-                this.missedShotLog.push(coord);
-                console.log('Missed!')
-                return "Miss!"
-            }
+    checkDefeat(){
+        if(this.casualties.length == 5){
+            console.log(`${this.name} LOST`)
+            return true
         }
     }
-
-    function tempPlace(player, start){
-        for(let i = 0; i < 5; i++){
-
-            player.placeShip(start + i, "vertical", player.ships[i])
+    ships = [
+        new Ship('Carrier', 5),
+        new Ship('Battleship', 4),
+        new Ship('Cruiser', 3),
+        new Ship('Submarine', 3),
+        new Ship('Destroyer', 2)
+    ]
+    placeShip(startIndex, direction, ship){
+        let board = this;
+        function checkValid(){
+            if(direction == "horizontal"){
+                if(board.grid[startIndex + ship.length] !== undefined){
+                    return true;
+                }
+            }
+            if(direction == "vertical"){
+                if(board.grid[(startIndex + 10) * ship.length] !== undefined){
+                    return true;
+                }
+            }
+            return false;
+        }
+        let lengthCount = ship.length;
+        let start = startIndex
+        while(lengthCount > 0){
+            this.grid[start].push(ship);
+            if(direction == 'horizontal' && checkValid()){
+                start++;
+            }
+            if(direction == 'vertical' && checkValid){
+                start = start + 10
+            }
+            lengthCount--;
         }
     }
+    missedShotLog = [];
+    casualties = [];
 
-    
-    const playerOne = new GameBoard('Player One');
-    const playerTwo = new GameBoard('Player Two');
-    
-    // tempPlace(playerOne, 0);
-    // tempPlace(playerTwo, 50);
+    receiveAttack(coord){
+        let ship = this.grid[coord][2];
+        if(typeof this.grid[coord][2] == 'object'){
+            ship.hit(); 
+            if(ship.sunk){
+                this.casualties.push(ship)
+            };
+            this.grid[coord].push('Hit!')
+            console.log('Hit!')
+            return "Hit!"
+        }
+        if(this.grid[coord][2]== undefined){
+            this.grid[coord][2] = 'miss'
+            this.missedShotLog.push(coord);
+            console.log('Missed!')
+            return "Miss!" 
+        }
+        else {
+            console.log('Coordinate already played...');
+            return;
+            
+        }
+    }
+}
 
-    // playerOne.receiveAttack(1);
-    // playerOne.receiveAttack(10);
-    // playerOne.receiveAttack(22);
-    // playerOne.receiveAttack(49);
-    // playerTwo.receiveAttack(0);
-    // playerTwo.receiveAttack(5);
+function tempPlace(player, start){
+    for(let i = 0; i < 5; i++){
 
-    module.exports = {playerOne, playerTwo}
+        player.placeShip(start + i, "vertical", player.ships[i])
+    }
+}
+
+
+const playerOne = new GameBoard('Player One');
+const playerTwo = new GameBoard('Player Two');
+
+tempPlace(playerOne, 0);
+tempPlace(playerTwo, 50);
+
+// playerOne.receiveAttack(1);
+// playerOne.receiveAttack(10);
+// playerOne.receiveAttack(22);
+// playerOne.receiveAttack(49);
+// playerTwo.receiveAttack(0);
+// playerTwo.receiveAttack(5);
+
+module.exports = {playerOne, playerTwo}
