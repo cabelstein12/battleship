32ddda7fd31e67359de91a1ef9c8df94cf804bee
cabelstein12 <Caleb.Steinwandt@gmail.com>
2024-04-24 05:29:38 -0600

    refactor/ cleanup index.js

--- src/index.js
+++ src/index.js
@@ -5,25 +5,15 @@ import { dir } from 'neo-async';
 function component(){
   let currentPlayer = playerOne;
   let nextPlayer = playerTwo;
+
   const content = document.querySelector('.content');
   const currentPlayerID = document.querySelector('#currentPlayerID');
         currentPlayerID.textContent = `${currentPlayer.name}, place your ships`;
 
-  const defenseGridElement = document.createElement('div');
-        defenseGridElement.setAttribute('id', 'defense');
-        
-  const offenseGridElement = document.createElement('div');
-        offenseGridElement.setAttribute('id', 'offense');
+  const defenseGridElement = document.getElementById('defense');
+  const offenseGridElement = document.getElementById('offense');
 
-        content.append(defenseGridElement);
-        content.append(offenseGridElement);
-
-  function createGrid(parent, name){
-      const gridID = document.createElement('div');
-            gridID.textContent = name;
-            gridID.classList.add('gridID');
-            parent.append(gridID);
-      
+  function createGrid(parent){
       const coordinateContainer = document.createElement('div');
             coordinateContainer.classList.add('grid');
             parent.append(coordinateContainer);
@@ -35,22 +25,23 @@ function component(){
           coordinateContainer.append(element);
       }
   }
-  createGrid(defenseGridElement, "Defensive Armada");
-  createGrid(offenseGridElement, "Enemy Waters");
+
+  createGrid(defenseGridElement);
+  createGrid(offenseGridElement);
   
   const defenseGrid = document.querySelector('#defense .grid');
   const attackGrid = document.querySelector('#offense .grid');
 
   function generateDefenseLayout(player){
     for(let i = 0 ; i < player.grid.length; i++){
+      if(player.grid[i].length == 4){
+        defenseGrid.children[i].classList.add('hit')
+      }
       if(typeof player.grid[i][2] === "object"){
         defenseGrid.children[i].classList.add('occupied');
       }
       if(typeof player.grid[i][2] === 'string'){
         defenseGrid.children[i].classList.add('miss');
-      }
-      if(player.grid[i].length == 4){
-        defenseGrid.children[i].classList.add('hit')
       }
     }
   }
@@ -98,42 +89,30 @@ function component(){
     currentPlayerID.textContent = `${currentPlayer.name}'s turn`;
     updateLogs();
   }
-  function startAttacks(){
-    attackCoordinates.forEach((e) => {
-      e.addEventListener('click', function(){
-          console.log(currentPlayer.name)
-          let target = parseInt(this.getAttribute('id'));
-          let attack = nextPlayer.receiveAttack(target);
-          if(nextPlayer.checkDefeat()){
-            console.log("GAME OVER", `${currentPlayer.name} Wins`);
-            updateLogs();
-            return;
-          }
-          if(attack !== "Try Again"){
-            changePlayer(); 
-          }
-          updateLogs();
-        })
-    })
-  } 
   
-  let shipDirection = 'vertical'
+  let shipDirection = 'vertical';
+
   function changeDirection(){
-    console.log(shipDirection)
     shipDirection == 'vertical' ? shipDirection = 'horizontal' : shipDirection = 'vertical'; 
-    directionButton.textContent = `Place ${shipDirection}ly`;
+    directionButton.textContent = `Placed ${shipDirection}ly`;
   }
+
   let directionButton = document.createElement('button');
-  directionButton.classList.add('directionButton');
-  directionButton.textContent = `Place ${shipDirection}ly`;
-  directionButton.addEventListener('click', changeDirection)
-  document.body.insertBefore(directionButton, content)
+      directionButton.classList.add('directionButton');
+      directionButton.textContent = `Place ${shipDirection}ly`;
+      directionButton.addEventListener('click', changeDirection);
+      document.body.insertBefore(directionButton, content);
+      
+  beginShipFormation(playerOne);
+  updateLogs();
+
   function beginShipFormation(player){
     currentPlayer = player
-
     currentPlayerID.textContent = `${player.name}, place your ships`;
     const controller = new AbortController();
     const { signal } = controller;
+    console.log(controller);
+
     defenseCoordinates.forEach((e) => {
       e.addEventListener('click', () => {addShipToUi(shipDirection, e, currentPlayer)}, {signal})
     })
@@ -154,18 +133,35 @@ function component(){
       console.log(`${player.name}'s ships in formation`, player.grid);
       controller.abort();
       refreshGrid();
-      changePlayer()
+      changePlayer();
       if(player == playerOne){
         beginShipFormation(playerTwo);
       }else{
         updateLogs();
-        console.log(player.name);
         directionButton.remove();
         startAttacks();
       }
     }
   }
-  beginShipFormation(playerOne);
-  updateLogs();
+
+  function startAttacks(){
+    attackCoordinates.forEach((e) => {
+      e.addEventListener('click', function(){
+          console.log(currentPlayer.name)
+          let target = parseInt(this.getAttribute('id'));
+          let attack = nextPlayer.receiveAttack(target);
+          if(nextPlayer.checkDefeat()){
+            console.log("GAME OVER", `${currentPlayer.name} Wins`);
+            updateLogs();
+            return;
+          }
+          if(attack !== "Try Again"){
+            changePlayer(); 
+          }
+          updateLogs();
+        })
+    })
+  } 
+
 }
 component();
